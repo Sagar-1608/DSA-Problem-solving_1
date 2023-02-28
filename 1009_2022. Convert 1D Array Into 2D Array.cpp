bf7fd32d772cf07f6394c46aa33e03fe4869f261
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:26:33 +0530

    Create 1009_2022. Convert 1D Array Into 2D Array.cpp

---
+++ 1009_2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,20 @@
+// 2022. Convert 1D Array Into 2D Array
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& arr, int col, int row) {
+
+        if (m * n != original.size())
+            return {};
+
+        vector<vector<int>> ret(m, vector<int>(n, 0));
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j)
+                ret[i][j] = original[col * i + j];
+
+                //formula positions[i][j]= num.ofcol in linear array *ith col + jth row  
+
+        }
+        return ret;
+    }
+};
