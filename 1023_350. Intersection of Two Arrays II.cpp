431624766f2a366126c729eb036d59fa790795db
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:37:27 +0530

    Create 1023_350. Intersection of Two Arrays II.cpp

---
+++ 1023_350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        class Solution ;
+    
+        map<int, int> mp1, mp2;
+        for(auto x: nums1) mp1[x]++;
+        for(auto x: nums2) mp2[x]++;
+        vector<int> ans;
+
+        for(auto i : mp1){
+            if(mp2[i.first]){
+                int count = min(i.second , mp2[i.first]);
+                while(count--)
+                {
+                    ans.push_back(i.first);
+                }
+            }
+        }
+        return ans; 
+
+         
+    }
+};
+        
+   
