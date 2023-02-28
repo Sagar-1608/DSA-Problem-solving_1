3a1ce9337644d07e7593f1ab42a04919916a157e
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:44:44 +0530

    Create 1032_80. Remove Duplicates from Sorted Array II.cpp

---
+++ 1032_80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,21 @@
+// 80. Remove Duplicates from Sorted Array II
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+
+    
+       int i=0;
+        for(auto e: arr)
+        {
+           if(i==0 || i==1 || arr[i-2]!=e )
+           {
+                arr[i]=e;
+                 i+=1;
+              
+           }
+        }
+        return i;
+        
+    }
+};
