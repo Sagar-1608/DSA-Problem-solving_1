6612f9012f2ce75d51af7d783f220af2221bf857
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:16:37 +0530

    Create 100-1385. Find the Distance Value Between Two Arrays.cpp

---
+++ 100-1385. Find the Distance Value Between Two Arrays.cpp
@@ -0,0 +1,36 @@
+1385. Find the Distance Value Between Two Arrays
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+
+    sort(arr2.begin(), arr2.end());  // nlog(n)
+    int c=0;
+
+    for(auto i : arr1)
+    {
+        int s = 0, e= arr2.size()-1,mid;
+        while(s<=e)
+        {
+            mid = s+(e-s)/2;
+            if(abs(i-arr2[mid])<=d)
+            {
+                c++;
+                break;
+            }
+            if(arr2[mid]<i)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+            
+
+        }
+    }
+     return (arr1.size()-c);
+    //  The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+        
+    }
+};
