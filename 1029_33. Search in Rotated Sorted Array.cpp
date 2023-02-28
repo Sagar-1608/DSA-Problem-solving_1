90b0f616184dccf657b870349363067c60361899
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:42:00 +0530

    Create 1029_33. Search in Rotated Sorted Array.cpp

---
+++ 1029_33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,28 @@
+// 33. Search in Rotated Sorted Array
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]== target) return mid;
+
+            else if(nums[mid]>=nums[low])
+            {
+                if(nums[mid]>= target and nums[low]<= target) high=mid-1;
+                else low = mid+1;
+            }
+            else{
+                if(nums[mid]<= target and nums[high]>= target) low=mid+1;
+                else high=mid-1;
+
+            }
+        }
+        return -1;
+        
+    }
+};
+
