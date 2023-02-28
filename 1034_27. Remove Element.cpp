098eb03522f7c2d7bf5917f17980056bdbc2863a
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:46:28 +0530

    Create 1034_27. Remove Element.cpp

---
+++ 1034_27. Remove Element.cpp
@@ -0,0 +1,18 @@
+// 27. Remove Element
+class Solution {
+public:
+    int removeElement(vector<int>& arr, int val) {
+        int i=0;
+        for(auto e: arr)
+        {
+           if(e!=val) 
+           {
+                arr[i]=e;
+                 i+=1;
+              
+           }
+        }
+        return i;
+        
+    }
+};
