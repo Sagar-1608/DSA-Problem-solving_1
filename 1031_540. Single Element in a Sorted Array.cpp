aab64907c02d29d168e09563641a0e62a3556459
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-28 22:43:54 +0530

    Create 1031_540. Single Element in a Sorted Array.cpp

---
+++ 1031_540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,20 @@
+// 540. Single Element in a Sorted Array
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n =arr.size();
+        int s =0;
+        int e = n-1,mid;
+
+        while(s<e)
+        {
+           mid = s+(e-s)/2;
+if((mid%2==0 && arr[mid]==arr[mid+1]) || (mid%2==1 && arr[mid]==arr[mid-1]))
+           s=mid+1;
+         else e=mid;
+
+        }
+        return arr[s];
+        
+    }
+};
