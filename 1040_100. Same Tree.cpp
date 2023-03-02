c0ec3fd6da6ed4a1f3d2514d2b4ac0339792c4b2
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-02 21:34:25 +0530

    Create 1040_100. Same Tree.cpp

---
+++ 1040_100. Same Tree.cpp
@@ -0,0 +1,37 @@
+// 100. Same Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+   bool cheak(TreeNode* p, TreeNode* q)
+   {
+       
+
+       
+    if(p == NULL && q == NULL) return true;
+    if(p == NULL || q == NULL || (p->val != q->val)) return false;
+
+    return cheak(p->left, q->left) && cheak(p->right, q->right);
+           
+      
+       
+       
+
+   }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        
+        return cheak(p,q);
+    }
+};
