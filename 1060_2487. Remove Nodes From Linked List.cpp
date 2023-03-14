ac4e34286e79e6d4710f5b793c18bf34263367d5
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-14 23:41:32 +0530

    Create 1060_2487. Remove Nodes From Linked List.cpp

---
+++ 1060_2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,59 @@
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+private:
+ListNode * reverse(ListNode * head)
+{
+    if(head==NULL || head->next==NULL) return head;
+
+    ListNode * chotahead = reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return chotahead;
+
+
+}
+public:
+    ListNode* removeNodes(ListNode* head) {
+
+        if(head==NULL || head->next==NULL) return head;
+        ListNode * rever;
+        rever=reverse(head);
+        ListNode *curr=rever;
+        ListNode * forw=rever->next;
+
+        while(forw)
+        {
+            if(curr->val>forw->val)
+            {
+                curr->next=forw->next;
+                forw=forw->next;
+
+            }
+           else
+           {
+                 curr=forw;
+            forw=forw->next;
+
+           }
+          
+
+
+        }
+
+        rever=reverse(rever);
+        return rever;
+
+        
+    }
+};
