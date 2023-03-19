78bc5281f0965e92bda19354ec2cee0cbb8c5ce3
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-19 18:51:24 +0530

    Create 1066_1472. Design Browser History.cpp

---
+++ 1066_1472. Design Browser History.cpp
@@ -0,0 +1,32 @@
+// 1472. Design Browser History
+class BrowserHistory {
+public:
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+        cur_idx = 0;
+    }
+    
+    void visit(string url) {
+        // Clear forward history
+        history.resize(cur_idx + 1);
+        history.push_back(url);
+        cur_idx++;
+    }
+    
+    string back(int steps) {
+        int prev_idx = max(0, cur_idx - steps);
+        cur_idx = prev_idx;
+        return history[cur_idx];
+    }
+    
+    string forward(int steps) {
+        int next_idx = min((int)history.size() - 1, cur_idx + steps);
+        cur_idx = next_idx;
+        return history[cur_idx];
+    }
+
+private:
+    vector<string> history;
+    int cur_idx;
+};
+
