5b1c608589d6388a61dab7d53bfeb1c6afa52c93
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-22 17:59:13 +0530

    Create 1070_2348. Number of Zero-Filled Subarrays.cpp

---
+++ 1070_2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,38 @@
+// 2348. Number of Zero-Filled Subarrays
+
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+
+    
+
+        // T(n) = O(n)
+        // S(n) = O(1)
+
+        
+        int n = nums.size();
+        long long count = 0, zeros = 0;
+        
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == 0) {
+                zeros++;  // increment the count of consecutive zeros
+            } 
+            else {
+                // calculate the number of subarrays of consecutive zeros and add to count
+                count += (zeros * (zeros + 1)) / 2;
+                zeros = 0;  // reset the count of consecutive zeros
+            }
+        }
+
+
+          // add the number of subarrays of consecutive zero of end of the array   
+        if (zeros > 0) {
+
+        count += (zeros * (zeros + 1))/2;
+        }
+        
+        return count;
+    }
+};
