9b6ba514d873ac0e4560632113a08ecadadc6a2a
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-03-31 20:16:35 +0530

    Create 1074_84. Largest Rectangle in Histogram.cpp

---
+++ 1074_84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,81 @@
+// 84. Largest Rectangle in Histogram
+
+
+class Solution {
+private: 
+vector<int> nextSmallIdx(vector<int> arr , int n )
+{
+    vector<int> ans(n);
+    stack<int> st ;
+    st.push(-1);
+
+    for(int i =n-1;i>=0;i--)
+    {
+        while(st.top()!=-1 && arr[st.top()]>=arr[i])
+        {
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+
+    }
+
+    return ans ;
+}
+
+vector<int> prevSmallIdx(vector<int> arr , int n )
+{
+    vector<int> ans(n);
+    stack<int> st ;
+    st.push(-1);
+
+    for(int i=0;i<n;i++)
+    {
+        while(st.top()!=-1 && arr[st.top()]>=arr[i])
+        {
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+
+    }
+
+    return ans ;
+}
+
+public:
+    int largestRectangleArea(vector<int>& heights) {
+
+        int n = heights.size();
+
+        
+
+         vector<int> next(n);
+         next = nextSmallIdx(heights,n);
+
+         vector<int> prev(n);
+         prev = prevSmallIdx(heights,n);
+
+         int area = INT_MIN;
+
+         for(int i= 0 ; i<n ; i++)
+         {
+             int l = heights[i];
+             if(next[i]==-1)
+             {
+                 next[i]=n;
+             }
+             int b = next[i]-prev[i] -1;
+
+             int newarea = l*b;
+
+             area= max(area,newarea);
+
+         }
+
+         return area;
+
+
+        
+    }
+};
