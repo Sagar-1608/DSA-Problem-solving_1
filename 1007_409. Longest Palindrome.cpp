2c9b6637a46852450c68c82923dbc5cc24085620
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-01 11:59:44 +0530

    adding  comment

--- 1007_409. Longest Palindrome.cpp
+++ 1007_409. Longest Palindrome.cpp
@@ -13,18 +13,18 @@ public:
       bool odd_is_found=false;
         for(auto i : mp)
         {
-            if((i.second)%2==0 )
+            if((i.second)%2==0 )  // adding the all even number of charactor 
             {
                 result+=i.second;
             }
             else{
-                result+= i.second-1;
+                result+= i.second-1;     // adding the odd charctor -1 
                 odd_is_found=true;
             }
 
         }
 
-        if(odd_is_found) result++;
+        if(odd_is_found) result++; // if odd is foind then add only one odd in result 
         return result;
     }
 };
