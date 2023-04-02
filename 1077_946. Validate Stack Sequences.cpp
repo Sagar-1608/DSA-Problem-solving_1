0093fa2a331ec02d023a10f351752072a9c2a95e
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-02 11:22:05 +0530

    Create 1077_946. Validate Stack Sequences.cpp

---
+++ 1077_946. Validate Stack Sequences.cpp
@@ -0,0 +1,44 @@
+// 946. Validate Stack Sequences
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& push, vector<int>& pop) {
+
+        // approach 1
+
+       /* stack<int> s;
+
+        for(int i = 0, j=0; i<push.size(); i++)
+        {
+            s.push(push[i]);
+
+            while(!s.empty() && s.top()==pop[j])
+            {
+                j++;
+                s.pop();
+            }
+
+        }
+
+        return s.empty();
+        */
+
+
+        //approach 2 with out stack 
+
+
+        int i =0 , j=0;
+
+        for(auto x : push)
+        {
+            push[i]=x;
+            i++;
+
+            while(i>0 && push[i-1]==pop[j])
+            {
+                j++;
+                i--;
+            }
+        }
+        return (i==0);
+    }
+};
