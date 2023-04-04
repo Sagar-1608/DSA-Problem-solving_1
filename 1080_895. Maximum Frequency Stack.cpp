91f9ca08212fcabeaa7c3374d3c5270dd69fa5c3
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-04 22:59:54 +0530

    Create 1080_895. Maximum Frequency Stack.cpp

---
+++ 1080_895. Maximum Frequency Stack.cpp
@@ -0,0 +1,36 @@
+// 895. Maximum Frequency Stack
+class FreqStack {
+
+    // by using hash map or priority_queue
+    // T(n)= log(n)
+    // S(n)= number of operation done in process;
+    priority_queue<pair<int,pair<int,int>>> pq;
+    unordered_map<int, int> freq;
+    int pos=0;
+public:
+
+
+    FreqStack() {
+        
+    }
+    
+    void push(int val) {
+        pq.push(make_pair(freq[val]++, make_pair(pos++,val)));
+        
+    }
+    
+    int pop() {
+        auto val = pq.top();
+        pq.pop();
+        int x = val.second.second;
+        freq[x]--;
+        return x;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
