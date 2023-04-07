644758ce761e14ff4edbbf47a92413c54928d682
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-07 20:48:08 +0530

    Create 1083_2405. Optimal Partition of String.cpp

---
+++ 1083_2405. Optimal Partition of String.cpp
@@ -0,0 +1,16 @@
+// 2405. Optimal Partition of String
+
+class Solution {
+public:
+    int partitionString(string s) {
+        int last_pos[26] = {}, partitions = 0, last_end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (last_pos[s[i] - 'a'] >= last_end) {
+                partitions++;
+                last_end = i + 1;
+            }
+            last_pos[s[i] - 'a'] = i + 1;
+        }
+        return partitions;
+    }
+};
