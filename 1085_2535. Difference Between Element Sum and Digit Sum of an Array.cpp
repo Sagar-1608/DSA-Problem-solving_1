c97bba77744fd5914b83745f74a96d5958e33c4d
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-09 23:30:57 +0530

    Create 1085_2535. Difference Between Element Sum and Digit Sum of an Array.cpp

---
+++ 1085_2535. Difference Between Element Sum and Digit Sum of an Array.cpp
@@ -0,0 +1,31 @@
+// 2535. Difference Between Element Sum and Digit Sum of an Array
+
+class Solution {
+    int digitSum(int a)
+    {   
+        int sum=0;
+        while(a!=0)
+        {
+            sum += a%10;
+            a=a/10;
+
+        }
+        return sum;
+    }
+public:
+    int differenceOfSum(vector<int>& nums) {
+        int digitsum=0;
+        int sum=0;
+        for(auto i : nums)
+        {
+            sum+=i;
+            digitsum+=digitSum(i);
+
+        }
+        int ans =abs(sum-digitsum);
+
+        return ans;
+        
+        
+    }
+};
