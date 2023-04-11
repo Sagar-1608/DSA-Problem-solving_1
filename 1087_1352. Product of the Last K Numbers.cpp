8cdedb8f14319191f3a1cb49a99f38e19c577bc5
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-11 23:23:21 +0530

    Create 1087_1352. Product of the Last K Numbers.cpp

---
+++ 1087_1352. Product of the Last K Numbers.cpp
@@ -0,0 +1,69 @@
+// 1352. Product of the Last K Numbers
+
+/*class ProductOfNumbers {
+    vector<int> arr;
+public:
+    ProductOfNumbers() {
+        
+    }
+    
+    void add(int num) {
+        arr.push_back(num);
+        
+    }
+    
+    int getProduct(int k) {
+        int n = arr.size();
+        int product=1;
+
+        for(int i=0; i<k;i++)
+        {
+            product *=arr[n-1];
+            n--;
+
+        }
+        return product;
+        
+    }
+};
+*/
+
+// in above method the thinme required is O(k)
+// reduce this time and do in o(1) time 
+// by using mentain the prefix product array 
+
+
+class ProductOfNumbers {
+    vector<int> arr;
+public:
+    ProductOfNumbers() {
+        arr.clear();
+        arr.push_back(1);
+        
+    }
+    
+    void add(int num) {
+      
+    //   creating the prefix product arr when addingg thr new element 
+     if(num==0)
+     {
+         arr.clear();
+         arr.push_back(1);
+     }
+     else
+     {
+         int last = arr.back(); //last element of array;
+         int current_product=last*num;
+         arr.push_back(current_product);
+     }
+        
+    }
+    
+    int getProduct(int k) {
+        int n = arr.size()-1;
+        if((n-k)<0) return 0;
+        else return(arr.back()/arr[n-k]);
+        
+    }
+};
+
