108320dd3a89474355ccbb3acc004ba7e5551cd6
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-12 23:27:07 +0530

    Create 1088_2390. Removing Stars From a String.cpp

---
+++ 1088_2390. Removing Stars From a String.cpp
@@ -0,0 +1,53 @@
+// 2390. Removing Stars From a String
+class Solution {
+public:
+    string removeStars(string s) {
+        /*
+        deque<char> q;
+
+        for(int i =0; i<s.length();i++)
+        {
+            char ch = s[i];
+            if(ch!='*')
+            {
+                q.push_back(ch);
+                
+
+            }
+            else
+            {
+                q.pop_back();
+               
+            }
+        }
+
+        s.clear();
+        while(!q.empty())
+        {
+            s.push_back(q.front());
+            q.pop_front();
+
+        }
+        return s;
+*/
+
+        // approach 2 without space
+
+        int j =0;
+
+        for(int i =0; i<s.length();i++)
+        {
+            if(s[i]!='*')
+            {
+                s[j]=s[i];
+                j++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return s.substr(0,j);
+        
+    }
+};
