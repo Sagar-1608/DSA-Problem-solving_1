772763d76f59b98d3e11f4c81523e8e394730876
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-04-25 22:15:49 +0530

    Update 1029_33. Search in Rotated Sorted Array.cpp

--- 1029_33. Search in Rotated Sorted Array.cpp
+++ 1029_33. Search in Rotated Sorted Array.cpp
@@ -5,6 +5,8 @@ public:
     int search(vector<int>& nums, int target) {
         int low=0;
         int high=nums.size()-1;
+        
+        //by using binary search 
         while(low<=high)
         {
             int mid=(low+high)/2;
