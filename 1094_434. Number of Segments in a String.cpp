7ee66798bb67f740881f09ffc9863b85c26a5927
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-05-14 22:24:23 +0530

    Create 1094_434. Number of Segments in a String.cpp

---
+++ 1094_434. Number of Segments in a String.cpp
@@ -0,0 +1,16 @@
+// 434. Number of Segments in a String
+
+class Solution {
+public:
+    int countSegments(string s) {
+
+        char prev=' ';
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=' ' && prev==' ' ) count++;
+            prev= s[i];
+        }
+
+        return count;
+    }
+};
