b1805994375e1d41f5d10ceaa787dbe37c25f57a
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-05-19 23:35:04 +0530

    Create 1098_2130. Maximum Twin Sum of a Linked List.cpp

---
+++ 1098_2130. Maximum Twin Sum of a Linked List.cpp
@@ -0,0 +1,62 @@
+// 2130. Maximum Twin Sum of a Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+
+        if(head==NULL) return 0;
+        int ans=0;
+
+
+        ListNode* slow = head;
+        ListNode* fast=head;
+
+        while(fast)
+        {
+            slow = slow->next;
+            fast=fast->next->next;
+        }
+
+
+
+       
+        ListNode* current = slow;
+        ListNode *prev = NULL, *next = NULL;
+ 
+        while (current)
+         {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        slow  = prev;
+
+
+
+     
+        while(slow)
+        {
+            int sum = head->val+slow->val;
+            ans=max(ans,sum);
+
+            slow = slow->next;
+            head = head->next;
+        }
+
+
+        return ans ;
+
+
+        
+    }
+};
