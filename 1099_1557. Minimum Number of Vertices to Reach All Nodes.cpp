9744749b4b40fc05dd800aebd50849744c2d44f3
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-05-25 23:35:28 +0530

    Update 1099_1557. Minimum Number of Vertices to Reach All Nodes.cpp

--- 1099_1557. Minimum Number of Vertices to Reach All Nodes.cpp
+++ 1099_1557. Minimum Number of Vertices to Reach All Nodes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
  vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
 
-
+//counting of edges present 
         vector<int> indegree(n, 0);
         for(auto edge : edges)
             indegree[edge[1]]++;
@@ -10,7 +10,7 @@ public:
         vector<int> ans;
         for(int i = 0;i < n; i++){
             if(indegree[i]==0)
-                ans.push_back(i);
+                ans.push_back(i);  // edge not present 
         }
 
         return ans;
