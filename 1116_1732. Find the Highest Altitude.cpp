fdc7053ce5338fd5af8a2f310d2f4057c00478ed
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-07-05 22:31:49 +0530

    Create 1116_1732. Find the Highest Altitude.cpp

---
+++ 1116_1732. Find the Highest Altitude.cpp
@@ -0,0 +1,20 @@
+// 1732. Find the Highest Altitude
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+        int ans = INT_MIN;
+        int preSum = 0;
+
+        for(int i=0;i<gain.size();i++){
+
+            ans = max(preSum,ans);
+             preSum += gain[i];
+        }
+        
+         if(preSum > ans)
+          ans = preSum;
+          
+       return ans; 
+    }
+};
