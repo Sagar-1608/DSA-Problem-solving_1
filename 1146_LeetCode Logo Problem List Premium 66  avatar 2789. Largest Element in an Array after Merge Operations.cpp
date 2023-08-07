e996a33a66a60ab630711e950414029c1f034be6
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-08-07 22:22:27 +0530

    Update 1146_LeetCode Logo Problem List Premium 66  avatar 2789. Largest Element in an Array after Merge Operations.cpp

--- 1146_LeetCode Logo Problem List Premium 66  avatar 2789. Largest Element in an Array after Merge Operations.cpp
+++ 1146_LeetCode Logo Problem List Premium 66  avatar 2789. Largest Element in an Array after Merge Operations.cpp
@@ -3,12 +3,15 @@ public:
     long long maxArrayValue(vector<int>& nums) {
          // nums = [2,3,7,9,3]
 
+        
+
         int n= nums.size();
         long long maxi=0;
         long long last = nums[n-1];
         
         for(int i=n-2;i>=0;i--)
         {
+            // check v is gerater or not 
             long long v = nums[i];
             if(v<=last){
                 last=last+v;
