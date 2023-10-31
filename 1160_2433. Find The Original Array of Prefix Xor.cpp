2576124c4146c95e3bb01e11a7c945630fad384a
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-10-31 18:28:32 +0530

    Create 1160_2433. Find The Original Array of Prefix Xor.cpp

---
+++ 1160_2433. Find The Original Array of Prefix Xor.cpp
@@ -0,0 +1,14 @@
+// 2433. Find The Original Array of Prefix Xor
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int prev = pref[0];
+        
+        for (int i = 1; i < pref.size(); i++) {
+            pref[i] ^= prev;
+            prev ^= pref[i];
+        }
+        
+        return pref;        
+    }
+};
