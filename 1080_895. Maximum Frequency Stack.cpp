72794b74b629cf043182f1de6c52acd38c098177
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2024-01-07 15:48:38 +0530

    Update 1080_895. Maximum Frequency Stack.cpp

--- 1080_895. Maximum Frequency Stack.cpp
+++ 1080_895. Maximum Frequency Stack.cpp
@@ -28,9 +28,4 @@ public:
     }
 };
 
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack* obj = new FreqStack();
- * obj->push(val);
- * int param_2 = obj->pop();
- */
+
