7fa06fc84e0ec7426e97dba685f6e9ba53327625
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2024-02-13 21:18:45 +0530

    Create 1202_2108. Find First Palindromic String in the Array.cpp

---
+++ 1202_2108. Find First Palindromic String in the Array.cpp
@@ -0,0 +1,14 @@
+// 2108. Find First Palindromic String in the Array
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(auto i:words){
+            string s=i;  // store the string s 
+            reverse(s.begin(),s.end());  //reverse it example string i=`aba` reverse it s='aba' is i and s is same then this is a palindrome
+            if(s==i){
+                return s;
+            }
+        }
+        return "";
+    }
+};
